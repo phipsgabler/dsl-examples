74bbacd0d5cf2e5a84435a84e83689364e504c0e
Philipp Gabler <phips.gabler@gmail.com>
2015-05-24 15:25:21 +0200

    Add license; extractors for logic ADT; more text in readme.

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
 # DSL-Examples #
 
 Here I implement various showcases of Scala features, mostly related to DSLs, to
-illustrate my bachelors's thesis.
+illustrate my bachelor's thesis.
+
+Every example consists of a module object, containing all necessary definitions,
+and a "test object" (inheriting from `App`), in which calls or constructions are
+shown to typecheck or to work.
+
+The "tests" should not be taken too seriously; they are only there to show how
+the defined features can be used, not to ensure any invariants.
+
+## Usage ##
+
+This is an SBT project; simply type `sbt console` on the top level. The package
+`dsl_examples` is automatically imported. To play around with individual features,
+you can import their module objects, like `import Logic._`.
+
+
+# Description of Individual Examples #
+
+## Imperative ##
+
+## Logic ##
+
+## MutableDict ##
+
+## Range ##
+
+## Read ##
+
+## Recover ##
+
+## Delay ##
--- src/main/scala/dsl_examples/Logic.scala
+++ src/main/scala/dsl_examples/Logic.scala
@@ -1,16 +1,18 @@
 package dsl_examples
 
+import scala.language.reflectiveCalls
+
 object Logic {
   sealed trait Expr {
-    def |(other: Expr) = Or(this, other)
-    def &(other: Expr) = And(this, other)
-    def unary_~ = Not(this)
+    def ||(other: Expr) = Or(this, other)
+    def &&(other: Expr) = And(this, other)
+    def unary_! = Not(this)
 
     def eval(env: Map[String, Boolean]): Option[Boolean] = this match {
       case Var(s) => env.get(s)
-      case Or(a, b) => for (x <- a.eval(env); y <- b.eval(env)) yield x || y
-      case And(a, b) => for (x <- a.eval(env); y <- b.eval(env)) yield x && y
-      case Not(a) => a.eval(env).map(!_)
+      case a || b => join(a.eval(env), b.eval(env)) by (_ || _) //for (x <- a.eval(env); y <- b.eval(env)) yield x || y
+      case a && b => join(a.eval(env), b.eval(env)) by (_ && _) //for (x <- a.eval(env); y <- b.eval(env)) yield x && y
+      case !(a) => a.eval(env).map(!_)
     }
   }
 
@@ -20,11 +22,45 @@ object Logic {
   case class Not(a: Expr) extends Expr
 
   implicit def stringToExpr(s: String): Var = Var(s)
+
+  object || {
+    def unapply(e: Expr): Option[(Expr, Expr)] = e match {
+      case Or(a, b) => Option(a, b)
+      case _ => None
+    }
+  }
+
+  object && {
+    def unapply(e: Expr): Option[(Expr, Expr)] = e match {
+      case And(a, b) => Option(a, b)
+      case _ => None
+    }
+  }
+
+  object ! {
+    def unapply(e: Expr): Option[Expr] = e match {
+      case Not(a) => Option(a)
+      case _ => None
+    }
+  }
+
+  // Quick and dirty; requires `reflectiveCalls`. Alternative: |@| from `scalaz`.
+  def join[A, B](a: Option[A], b: Option[B]) = new {
+    def by[C](f: (A, B) => C): Option[C] = for {
+      x <- a
+      y <- b
+    } yield f(x, y)
+  }
 }
 
-object LogicTest {
+object LogicTest extends App {
   import Logic._
 
-  val e1: Expr = "a" | "b" & ~"c"
+  val e1: Expr = "a" || "b" && !"c"
   assert(e1.eval(Map("a" -> true, "b" -> false, "c" -> true)) == Some(true))
+
+  e1 match {
+    case e@(Var("a") || Var("b") && !(Var("c"))) => println(s"Success: $e")
+    case _ => println("Failure")
+  }
 }
